c857e142cb9749dec3e0d4c41daa3108305ec81e
Jeremy-Samaroo <56980709+Jeremy-Samaroo@users.noreply.github.com>
2021-12-15 20:17:15 -0400

    Created the fragment view for report form

---
+++ app/src/main/java/com/example/potholespotterse2/Camera_Report.java
@@ -0,0 +1,25 @@
+package com.example.potholespotterse2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Camera_Report extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera__report, container, false);
+    }
+}
